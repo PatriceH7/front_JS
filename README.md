af90d7a9c9b713025f5512fd88c1f867144b391f
PatriceH <patricehuszezo@gmail.com>
2020-11-17 05:08:34 +0100

    Commit fonctionnalité 6 et README

---
+++ README.md
@@ -0,0 +1,15 @@
+# README for Events on JS
+
+Ce projet est un nouvel exercice sur le Javascript, afin d'explorer
+les fonctionnalités permettant de rendre une page interactive
+
+Cet exercice se fait sans modifier le fichier html donné.
+Pour le lancer, merci de lancer le fichier html, puis observer les fonctionnalités suivantes, avec la console JS.
+
+Fonctionnalité 1 -bis : cliquer sur le footer
+Fonctionnalité 2 : cliquer sur le hamburger menu en haut à droite
+Fonctionnalité 3 : cliquer sur le bouton "Edit" de la première card
+Fonctionnalité 4 : cliquer sur le bouton "Edit" de la seconde card
+Fonctionnalité 5 : cliquer sur la navbar en haut
+Fonctionnalité 6 : passer la souris sur le bouton "view"
+
--- script.js
+++ script.js
@@ -42,8 +42,61 @@ console.log(cardText1);
 var buttonEditCard2 = document.getElementsByClassName("btn btn-sm btn-outline-secondary")[1];
 var cardText2 = document.getElementsByClassName("card-text")[1];
 var onCard2EditClick = function() {
-    cardText2.style.color = "green";
+    if (cardText2.style.color === "green") {
+        cardText2.style.color = "";
+    } else cardText2.style.color = "green";
 }
 buttonEditCard2.addEventListener("click", onCard2EditClick);
 console.log(buttonEditCard2);
 console.log(cardText2);
+
+// Fonctionnalité 5
+var navBar = document.getElementsByClassName("navbar navbar-dark bg-dark box-shadow")[0];
+var linkEl = document.querySelector("link")
+var onNavbarDblClick = function() {
+    if (linkEl.href === "https: //stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css") {
+        linkEl.href = "";
+    }
+};
+navBar.addEventListener("dblclick", onNavbarDblClick);
+
+var onDocDblClick = function() {
+    if (linkEl.href = "") {
+        linkEl.href = "https: //stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css";
+    }
+}
+document.addEventListener("dblclick", onDocDblClick);
+console.log(navBar);
+console.log(linkEl.href);
+
+// Fonctionnalité 6
+var buttonViewCard = document.getElementsByClassName("btn btn-sm btn-success")[0];
+var card = document.getElementsByClassName("col-md-4")[0];
+var overButtonViewCard = function() {
+    if (card.getElementsByTagName("img")[0].style.width === "20%") {
+        card.getElementsByTagName("img")[0].style.width = "";
+        card.getElementsByClassName("card-text")[0].classList.toggle("collapse");
+    } else {
+        card.getElementsByTagName("img")[0].style.width = "20%";
+        card.getElementsByClassName("card-text")[0].classList.toggle("collapse");
+    }
+}
+buttonViewCard.addEventListener("mouseover", overButtonViewCard);
+console.log(buttonViewCard);
+console.log(card);
+/*
+var buttonViewCards = document.getElementsByClassName("btn btn-sm btn-success");
+var cards = document.getElementsByClassName("col-md-4");
+
+for (let i = 0; i < 6; i++) {
+    var overButtonViewCard = function() {
+        if (cards[i].getElementsByTagName("img")[i].style.width === "20%") {
+            cards[i].getElementsByTagName("img")[i].style.width = "";
+            cards[i].getElementsByClassName("card-text")[i].classList.toggle("collapse");
+        } else {
+            cards[i].getElementsByTagName("img")[i].style.width = "20%";
+            cards[i].getElementsByClassName("card-text")[i].classList.toggle("collapse");
+        }
+    }
+    buttonViewCards[i].addEventListener("mouseover", overButtonViewCard());
+}*/
